ca407629dd53100731f76794e7db7719f83d0292
makiomar <mo7amed.maki@gmail.com>
2020-12-14 18:24:28 +0200

    Implement transient with oauth

--- meetings/oauth.php
+++ meetings/oauth.php
@@ -7,57 +7,69 @@ function anony_outh_callback(){
 	
 	extract($_GET);
 	
-	$appointment_IDs = $state;
+	$appointment_data = explode('-', $state);
 	
-	return $appointment_IDs;
+	$current_user_id = get_current_user_id();
 	
-	$request = 
-		[
-	        "headers" => [
-	            "Authorization" => "Basic ". base64_encode(CLIENT_ID.':'.CLIENT_SECRET)
-	        ],
-	        'form_params' => [
-	            "grant_type" => "authorization_code",
-	            "code" => @$_GET['code'],
-	            "redirect_uri" => REDIRECT_URI
-	        ],
-	    ];
-	
-	try {
-	    $client = new GuzzleHttp\Client(['base_uri' => 'https://zoom.us']);
-	 
-	    $response = $client->request('POST', '/oauth/token', $request);
-	 
-	    $token = json_decode($response->getBody()->getContents(), true);
-	 
-	    $zoom_token = new ANONY_Zoom_Token();
-	 	
-	 	$zoom_token->updateAccessToken($token);
+	if(get_transient('zoom_temp_'.$current_user_id.'_'.$appointment_data[1]) == $current_user_id || current_user_can('administrator')){
 	    
-	} catch(Exception $e) {
-		
-	   if( 401 == $e->getCode() ) {
-            $refresh_token = $accessToken;
- 
-            $client = new GuzzleHttp\Client(['base_uri' => 'https://zoom.us']);
-            $response = $client->request('POST', '/oauth/token', [
-                "headers" => [
-                    "Authorization" => "Basic ". base64_encode(CLIENT_ID.':'.CLIENT_SECRET)
-                ],
-                'form_params' => [
-                    "grant_type" => "refresh_token",
-                    "refresh_token" => $refresh_token
-                ],
-            ]);
-            
-            $zoom_token->updateAccessToken($response->getBody());
- 
-            anony_create_meeting();
-        } else {
-            return $e->getMessage();
-        }
-	    
+	    if(intval($current_user_id) === intval($appointment_data[2]) || current_user_can('administrator')){
+	        
+    	    print_r($appointment_data);
+    	    
+        	$request = 
+        		[
+        	        "headers" => [
+        	            "Authorization" => "Basic ". base64_encode(CLIENT_ID.':'.CLIENT_SECRET)
+        	        ],
+        	        'form_params' => [
+        	            "grant_type" => "authorization_code",
+        	            "code" => @$_GET['code'],
+        	            "redirect_uri" => REDIRECT_URI
+        	        ],
+        	    ];
+        	
+        	try {
+        	    $client = new GuzzleHttp\Client(['base_uri' => 'https://zoom.us']);
+        	 
+        	    $response = $client->request('POST', '/oauth/token', $request);
+        	 
+        	    $token = json_decode($response->getBody()->getContents(), true);
+        	 
+        	    $zoom_token = new ANONY_Zoom_Token();
+        	 	
+        	 	$zoom_token->updateAccessToken($token);
+        	    
+        	} catch(Exception $e) {
+        		
+        	   if( 401 == $e->getCode() ) {
+                    $refresh_token = $accessToken;
+         
+                    $client = new GuzzleHttp\Client(['base_uri' => 'https://zoom.us']);
+                    $response = $client->request('POST', '/oauth/token', [
+                        "headers" => [
+                            "Authorization" => "Basic ". base64_encode(CLIENT_ID.':'.CLIENT_SECRET)
+                        ],
+                        'form_params' => [
+                            "grant_type" => "refresh_token",
+                            "refresh_token" => $refresh_token
+                        ],
+                    ]);
+                    
+                    $zoom_token->updateAccessToken($response->getBody());
+         
+                    anony_create_meeting();
+                } else {
+                    return $e->getMessage();
+                }
+        	    
+        	}	        
+	    }
 	}
+	
+	delete_transient('zoom_temp_'.$current_user_id.'_'.$appointment_data[1]);
+	
+
 }
 
 add_shortcode( 'anony-zoom-meeting', 'anony_outh_callback' );
